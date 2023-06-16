619f27ecc87c0a748f46936c9fca908ce62424a4
GersonRS <grs@cesar.org.br>
2023-06-16 19:11:49 -0300

    Add resolution

--- PyFinanceiro/main.py
+++ PyFinanceiro/main.py
@@ -1,1 +1,37 @@
+import sys
 
+data = []
+lp = []
+# armazena a data em um vetor e o lucro/prejuízo em outro
+with open("PyFinanceiro/recursos/dados_financeiro.txt") as dados:
+    for line in dados.readlines()[1:]:
+        data.append(line.split(",")[0])
+        lp.append(int(line.split(",")[1]))
+
+variacao = []
+i = 0
+# calcula as variações entre os meses
+while i < len(lp) - 1:
+    variacao.append(lp[i + 1] - lp[i])
+    i += 1
+
+total_meses = len(data)
+total = sum(lp)
+
+# arquivo de saída
+with open("PyFinanceiro/relatório.txt", "w") as result:
+    temp = sys.stdout  # armazena em uma variável temporária o stdout padrão
+    sys.stdout = result  # troca o stdout para o arquivo
+    print("Analise financeira")
+    print("-" * 28)
+    print(f"Total de meses: {total_meses}")
+    print(f"Total: $ {total}")
+    print(f"Média: $ {total/total_meses:.2f}")
+    print(f"Variação da média: $ {sum(variacao)/len(variacao):.2f}")
+    print(
+        f"Maior aumento nos lucros: {data[variacao.index(max(variacao)) + 1]} ($ {max(variacao)})"
+    )
+    print(
+        f"Maior redução nos lucros: {data[variacao.index(min(variacao)) + 1]} ($ {min(variacao)})"
+    )
+    sys.stdout = temp  # devolve o stdout padrão
--- PyVotacao/main.py
+++ PyVotacao/main.py
@@ -1,1 +1,33 @@
+import sys
 
+votos = []
+# cria uma lista apenas com os candidados que receberam votos
+with open("PyVotacao/recursos/dados_elecao.txt") as dados:
+    for linha in dados.readlines()[1:]:
+        votos.append(linha.split(",")[2].replace("\n", ""))
+
+candidatos = set(votos)  # cria um set, com o nome de cada candidato uma vez
+cand_voto = dict()
+# cria um dicionário com 'nome candidato' x 'quantidade votos'
+for candidato in candidatos:
+    cand_voto[candidato] = votos.count(candidato)
+
+# organiza o dicionário usando uma função persolanizada
+# P.S.: confira um material extra na próxima cela
+cand_voto = sorted(cand_voto.items(), key=lambda x: x[1], reverse=True)
+
+# cria o arquivo com os resultados
+with open("PyVotacao/resultado.txt", "w") as result:
+    temp = sys.stdout
+    sys.stdout = result
+
+    print("Resultados eleitorais")
+    print("-" * 25)
+    print(f"Total de votos: {len(votos)}")
+    print("-" * 25)
+    for cand in cand_voto:
+        print(f"{cand[0]}: {(cand[1] * 100)/len(votos):.1f}% ({cand[1]})")
+    print("-" * 25)
+    print(f"Vencedor: {cand_voto[0][0]}")
+    print("-" * 25, end="")
+    sys.stdout = temp
